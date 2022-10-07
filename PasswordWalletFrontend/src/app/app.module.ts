ee086802e927b59519660702def30b7e99d3d584
bajdaprzemyslaw <przemek.android20@gmail.com>
2022-10-07 16:02:26 +0200

    add login page

--- PasswordWalletFrontend/src/app/app.module.ts
+++ PasswordWalletFrontend/src/app/app.module.ts
@@ -3,15 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import { LoginComponent } from './component/login/login.component';
+import {MatButtonModule} from "@angular/material/button";
+import {MatCardModule} from "@angular/material/card";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {MatInputModule} from "@angular/material/input";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatFormFieldModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatCardModule,
+        NgbModule,
+        MatInputModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
